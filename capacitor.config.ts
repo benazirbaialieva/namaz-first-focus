3b2a828cacab2d16fea883622305d86bbe71324e
Benazir Baialieva <benazir.baialieva@gmail.com>
2026-03-17 16:22:00 -0700

    changes to run app on local

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.codewise.namazfirst',
   appName: 'Salah Guard',
-  webDir: 'dist',
-  server: {
-    url: 'https://981b6eae-0a1c-4584-9968-9cd367ab8212.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  webDir: 'dist'
 };
 
 export default config;
